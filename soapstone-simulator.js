40002c256ade526aacad9d02f18c19e8c37d35fb
Adam Miller <luphytwo@gmail.com>
2015-11-21 14:01:31 -0500

    Basic design of the DS1 soapstone menu complete

--- soapstone-simulator.js
+++ soapstone-simulator.js
@@ -5,22 +5,25 @@ var suffixes;
 var phraseText;
 
 function create(game) {
-	// Load phrase
+	// Load phrase div
 	phraseText = document.getElementById("phraseText");
 
-	// Load prefixes, suffixCategories, and suffixes divs	
+	// Load prefixes, suffixCategories, and suffixes tables	
 	prefixes = document.getElementById("prefixes");
 	suffixCategories = document.getElementById("suffixCategories");
 	suffixes = document.getElementById("suffixes");
 
 	for(var i = 0; i < game.prefixes.length; ++i)
 	{
-		// Create prefix divs
-		var prefix = document.createElement("div");
-		var prefixText = document.createTextNode(game.prefixes[i]);
+		// Create prefix row and data elements
+		var prefix = document.createElement("tr");
+		var prefixText = document.createElement("td");
 
-		// Set ID
-		prefix.id = "menuItem";
+		// Assign it text
+		prefixText.innerHTML = game.prefixes[i];
+
+		// Set class
+		prefixText.className = "menuItem";
 
 		// Make the index of the prefix a member of the element as well as the type
 		// This is so that it can be accessed in the listener function
@@ -50,25 +53,36 @@ function create(game) {
 
 function loadSuffixCategories(game, prefixTypeIndex, prefixIndex)
 {
+	// Unhide
 	suffixCategories.style.display = "block";
 
 	for(var i = 0; i < game.suffixCategories.length; ++i)
 	{
-		var suffixCategory = document.createElement("div");
-		var suffixCategoryText = document.createTextNode(game.suffixCategories[i]);
+		// Create suffixCategory row and data elements
+		var suffixCategory = document.createElement("tr");
+		var suffixCategoryText = document.createElement("td");
 
-		suffixCategory.id = "menuItem";
+		// Assign it text
+		suffixCategoryText.innerHTML = game.suffixCategories[i];
 
+		// Set class
+		suffixCategoryText.className = "menuItem";
+
+		// Make the prefix index, type index, and category index members of the element
 		suffixCategory.prefixIndex = prefixIndex;
 		suffixCategory.prefixTypeIndex = prefixTypeIndex;
 		suffixCategory.index = i;
 
+		// click listener for it
 		suffixCategory.addEventListener("click", function() {
+			// Clear the suffix div if it has anything in it
 			clearSuffixes();
 
+			// Load the category
 			loadSuffixCategory(game, this.prefixIndex, this.prefixTypeIndex, this.index);
 		}, false);
 
+		// Append elements
 		suffixCategory.appendChild(suffixCategoryText);
 		suffixCategories.appendChild(suffixCategory);
 	}
@@ -76,28 +90,40 @@ function loadSuffixCategories(game, prefixTypeIndex, prefixIndex)
 
 function loadSuffixCategory(game, prefixIndex, prefixTypeIndex, suffixCategoryIndex)
 {
+	// Unhide it
 	suffixes.style.display = "block";
 
 	for(var i = 0; i < game.suffixes[prefixTypeIndex][suffixCategoryIndex].length; ++i)
 	{
-		var suffix = document.createElement("div");
-		var suffixText = document.createTextNode(game.suffixes[prefixTypeIndex][suffixCategoryIndex][i]);
+		// Create suffix row and data elements
+		var suffix = document.createElement("tr");
+		var suffixText = document.createElement("td");
 
-		suffix.id = "menuItem";
+		// Assign it text
+		suffixText.innerHTML = game.suffixes[prefixTypeIndex][suffixCategoryIndex][i];
 
+		// Set class
+		suffixText.className = "menuItem";
+
+		// Make the prefix index, type index, category index, and suffix index members of the element
 		suffix.prefixIndex = prefixIndex;
 		suffix.prefixTypeIndex = prefixTypeIndex;
 		suffix.suffixCategoryIndex = suffixCategoryIndex
 		suffix.index = i;
 
+		// Mouseover listener for the phrase
 		suffix.addEventListener("mouseover", function() {
+			// Set the phrase text to the option being moused over
 			phraseText.innerHTML = game.prefixes[this.prefixIndex].replace("****", game.suffixes[this.prefixTypeIndex][this.suffixCategoryIndex][this.index]);
 		}, false);
 
+		// Click listener
 		suffix.addEventListener("click", function() {
+			// Reset the menus
 			reset();
 		}, false);
 
+		// Append elements
 		suffix.appendChild(suffixText);
 		suffixes.appendChild(suffix);
 	}
